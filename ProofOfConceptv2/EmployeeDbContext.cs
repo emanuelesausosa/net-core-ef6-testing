819244f1684700cfd8978d1ec931651d7cb43e3b
Emanuel Sosa <emanuelesausosa@gmail.com>
2019-08-22 11:04:59 -0600

    first commit add Employee entity and migrations

---
+++ ProofOfConceptv2/EmployeeDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProofOfConceptv2.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProofOfConceptv2
+{
+    public class EmployeeDbContext: DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        DbSet<Employee> Employees { get; set; }
+    }
+}
